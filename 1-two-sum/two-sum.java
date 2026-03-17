c548f04abe715fed6b88dd0fc99d923c9ef62e29
Carlos Ignacio Ramirez Perez <cigramirez@poligran.edu.co>
2026-03-17 17:54:41 -0500

    Time: 49 ms (9.03%) | Memory: 46.9 MB (88.27%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,10 +2,8 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] solucion = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            int primerNumero = nums[i];
             for (int j = i+1; j < nums.length; j++) {
-                int segundoNumero = nums[j];
-                if (primerNumero + segundoNumero == target) {
+                if (nums[i] + nums[j] == target) {
                     solucion[0] = i;
                     solucion[1] = j;
                 }
